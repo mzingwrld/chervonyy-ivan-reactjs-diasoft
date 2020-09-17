20420ebb69f593fd25be0129bfab2fac6e53d6c7
Червоный Иван <ichervonyy@diasoft.ru>
2020-09-17 14:08:19 +0300

    added untracked files

---
+++ reactjs/todo/src/components/TodoContainer/StyledTodo.js
@@ -0,0 +1,10 @@
+import {Modal, Input} from 'antd';
+import styled from 'styled-components';
+
+const {TextArea} = Input;
+
+export const StyledTextArea = styled(TextArea)`
+  background: #fff;
+`;
+
+export const StyledModal = styled(Modal)``;
